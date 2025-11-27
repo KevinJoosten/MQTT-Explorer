03c4d2586cfc900b6df501415977b03e4fc51775
Kevin Joosten <kevin.joosten@marel.com>
2025-11-27 23:35:27 +0100

    Add folder management state: collapsedFolders, folderOrder, connectionOrder

--- app/src/reducers/ConnectionManager.ts
+++ app/src/reducers/ConnectionManager.ts
@@ -10,6 +10,9 @@ export interface ConnectionManagerState {
   showAdvancedSettings: boolean
   showCertificateSettings: boolean
   sortBy: SortOption
+  collapsedFolders: { [folderName: string]: boolean }
+  folderOrder: string[]
+  connectionOrder: { [folderName: string]: string[] }
 }
 
 const initialState: ConnectionManagerState = {
@@ -17,7 +20,10 @@ const initialState: ConnectionManagerState = {
   selected: undefined,
   showAdvancedSettings: false,
   showCertificateSettings: false,
-  sortBy: 'name',
+  sortBy: 'folder',
+  collapsedFolders: {},
+  folderOrder: [],
+  connectionOrder: {},
 }
 
 export type Action =
@@ -31,6 +37,9 @@ export type Action =
   | DeleteSubscription
   | AddSubscription
   | SetSortBy
+  | ToggleFolderCollapse
+  | SetFolderOrder
+  | SetConnectionOrder
 
 export enum ActionTypes {
   CONNECTION_MANAGER_SET_CONNECTIONS = 'CONNECTION_MANAGER_SET_CONNECTIONS',
@@ -43,6 +52,9 @@ export enum ActionTypes {
   CONNECTION_MANAGER_ADD_SUBSCRIPTION = 'CONNECTION_MANAGER_ADD_SUBSCRIPTION',
   CONNECTION_MANAGER_DELETE_SUBSCRIPTION = 'CONNECTION_MANAGER_DELETE_SUBSCRIPTION',
   CONNECTION_MANAGER_SET_SORT_BY = 'CONNECTION_MANAGER_SET_SORT_BY',
+  CONNECTION_MANAGER_TOGGLE_FOLDER_COLLAPSE = 'CONNECTION_MANAGER_TOGGLE_FOLDER_COLLAPSE',
+  CONNECTION_MANAGER_SET_FOLDER_ORDER = 'CONNECTION_MANAGER_SET_FOLDER_ORDER',
+  CONNECTION_MANAGER_SET_CONNECTION_ORDER = 'CONNECTION_MANAGER_SET_CONNECTION_ORDER',
 }
 
 export interface SetConnections {
@@ -96,6 +108,22 @@ export interface SetSortBy {
   sortBy: SortOption
 }
 
+export interface ToggleFolderCollapse {
+  type: ActionTypes.CONNECTION_MANAGER_TOGGLE_FOLDER_COLLAPSE
+  folderName: string
+}
+
+export interface SetFolderOrder {
+  type: ActionTypes.CONNECTION_MANAGER_SET_FOLDER_ORDER
+  folderOrder: string[]
+}
+
+export interface SetConnectionOrder {
+  type: ActionTypes.CONNECTION_MANAGER_SET_CONNECTION_ORDER
+  folderName: string
+  connectionOrder: string[]
+}
+
 export const connectionManagerReducer = createReducer(initialState, {
   CONNECTION_MANAGER_SET_CONNECTIONS: setConnections,
   CONNECTION_MANAGER_SELECT_CONNECTION: selectConnection,
@@ -104,9 +132,12 @@ export const connectionManagerReducer = createReducer(initialState, {
   CONNECTION_MANAGER_DELETE_CONNECTION: deleteConnection,
   CONNECTION_MANAGER_TOGGLE_ADVANCED_SETTINGS: toggleAdvancedSettings,
   CONNECTION_MANAGER_TOGGLE_CERTIFICATE_SETTINGS: toggleCertificateSettings,
-  CONNECTION_MANAGER_DELETE_SUBSCRIPTION: deleteSubscription,
   CONNECTION_MANAGER_ADD_SUBSCRIPTION: addSubscription,
+  CONNECTION_MANAGER_DELETE_SUBSCRIPTION: deleteSubscription,
   CONNECTION_MANAGER_SET_SORT_BY: setSortBy,
+  CONNECTION_MANAGER_TOGGLE_FOLDER_COLLAPSE: toggleFolderCollapse,
+  CONNECTION_MANAGER_SET_FOLDER_ORDER: setFolderOrder,
+  CONNECTION_MANAGER_SET_CONNECTION_ORDER: setConnectionOrder,
 })
 
 function setConnections(state: ConnectionManagerState, action: SetConnections): ConnectionManagerState {
@@ -239,5 +270,32 @@ function setSortBy(state: ConnectionManagerState, action: SetSortBy): Connection
   return {
     ...state,
     sortBy: action.sortBy,
+  }
+}
+
+function toggleFolderCollapse(state: ConnectionManagerState, action: ToggleFolderCollapse): ConnectionManagerState {
+  return {
+    ...state,
+    collapsedFolders: {
+      ...state.collapsedFolders,
+      [action.folderName]: !state.collapsedFolders[action.folderName],
+    },
+  }
+}
+
+function setFolderOrder(state: ConnectionManagerState, action: SetFolderOrder): ConnectionManagerState {
+  return {
+    ...state,
+    folderOrder: action.folderOrder,
+  }
+}
+
+function setConnectionOrder(state: ConnectionManagerState, action: SetConnectionOrder): ConnectionManagerState {
+  return {
+    ...state,
+    connectionOrder: {
+      ...state.connectionOrder,
+      [action.folderName]: action.connectionOrder,
+    },
   }
 }
